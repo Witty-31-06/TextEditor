a5da7ed17321984862caa773a6aa10956778b985
Witty-31-06 <92244610+Witty-31-06@users.noreply.github.com>
2024-01-29 10:30:10 +0530

    added the control for scrolling

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -7,28 +11,24 @@
 #include <errno.h>
 #include <sys/ioctl.h>
 #define VER "1.0"
-//this macro generates the ascii value of the control sequence eg-ctrl+a - 1 ctrl+m - 13 etc
 typedef struct termios terminal;
 
-struct _editorState
+//This struct contains row of text
+typedef struct erow
 {
-	int cx, cy;
-	unsigned short int screenrows;
-	unsigned short int screencols;
-	terminal original;
-};
-typedef struct _editorState editorState;
-editorState config;
+	int size;
+	char *chars;
+} erow;
 
-
-typedef struct {
+/*This struct contains the buffer which we have to output in terminal*/
+typedef struct abuf{
 	char *s;
 	int len;
 } abuf;
 #define ABUF_INIT {NULL, 0}
 /**---terminal---**/
 
-typedef enum {
+typedef enum keys{
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT = 1001,
 	ARROW_UP = 1002,
@@ -39,6 +39,27 @@ typedef enum {
 	END = 1007,
 	DEL = 1008,
 } splKeys;
+
+
+typedef struct estate
+{
+	int cx, cy;
+	unsigned short int screenrows;
+	unsigned short int screencols;
+	int numrow;
+	int colOff;
+	int rowOff; //for scrolling
+	erow *row;
+	terminal original;
+} editorState;
+editorState config;
+
+
+/**
+ * @brief Outputs the error in screen and exits
+ * 
+ * @param s 
+ */
 void err(const char *s)
 {
 
@@ -59,6 +80,11 @@ void exitRawMode()
 	}	
 }
 
+
+/**
+ * @brief Enables raw mode of the terminal
+ * 
+ */
 void enableRawMode()
 {
 	terminal raw;
@@ -83,8 +109,57 @@ void enableRawMode()
 
 }
 
+/**row operations**/
+
+void appendRow(char *s, size_t len)
+{
+	//Adding one extra space for new row
+	config.row = realloc(config.row, (sizeof(erow)) * (config.numrow+1));
+
+	//Inserting the row info at the specific index
+	int ind = config.numrow;
+	config.row[ind].size = len;
+	config.row[ind].chars = malloc(len+1);
+	strncpy((config.row[ind].chars), s, len);
+	config.numrow++;
+
+}
+/**FILE I/O**/
+
+/**
+ * @brief This function opens the file
+ * 
+ * @param filename 
+ */
+void editorOpen(char *filename)
+{
+	FILE *fp = fopen(filename, "r");
+	if(!fp) err("fopen");
+
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	while((linelen = getline(&line, &linecap, fp)) != -1)
+	{
+		while(linelen > 0 && (line[linelen-1] == '\r' || line[linelen-1] == '\n'))
+		{
+			linelen--;
+		}
+		appendRow(line, linelen);
+	}
+	
+	free(line);
+	fclose(fp);
+}
 /**Append Buffer**/
 
+/**
+ * @brief This function appends the string to the buffer
+ * 
+ * @param ab 
+ * @param s 
+ * @param len 
+ */
 void abAppend(abuf *ab, const char *s, int len)
 {
 	char *new = realloc(ab->s, ab->len+len); // allocating space for increasing size of string
@@ -96,10 +171,23 @@ void abAppend(abuf *ab, const char *s, int len)
 
 }
 
+/**
+ * @brief This function frees the buffer
+ * 
+ * @param ab 
+ */
 void abFree(abuf *ab)
 {
 	free(ab->s);
 }
+
+/**----input----**/
+
+/**
+ * @brief This function reads the keypress
+ * 
+ * @return int 
+ */
 int readKey()
 {
 	int nread;
@@ -157,8 +245,13 @@ int readKey()
     	return (int)c;
 	}
 }
-/**----input----**/
 
+
+/**
+ * @brief This function moves the cursor
+ * 
+ * @param key 
+ */
 void moveCursor(int key)
 {
 	switch(key)
@@ -170,18 +263,21 @@ void moveCursor(int key)
 			if(config.cy != 0) config.cy--;
 			break;
 		case ARROW_DOWN:
-			if(config.cy != config.screenrows-1) config.cy++;
+			if(config.cy < config.numrow) config.cy++;
 			break;
 		case ARROW_RIGHT:
-			if(config.cx != config.screencols) config.cx++;
+			config.cx++;
 			break;
 	}
 }
+
+/**
+ * @brief This function processes the keypress
+ * 
+ */
 void processKeyPress()
 {
 	int c = readKey();
-	char k[1];
-	k[0] = (char)c;
 	switch(c)
 	{
 		//present in ttydefaults already included
@@ -209,16 +305,17 @@ void processKeyPress()
 			moveCursor(c);
 			break;
 		default:
-			write(STDOUT_FILENO, k, 1);
 			break;
 	}
 }
 
-/**----output----**/
-
-/*
-
-*/
+/**
+ * @brief This function gets the cursor position
+ * 
+ * @param row 
+ * @param col 
+ * @return int
+ * */
 int getCursorPosition(unsigned short int *row, unsigned short int *col)
 {
 	*row = *col = 0;
@@ -252,6 +349,10 @@ int getCursorPosition(unsigned short int *row, unsigned short int *col)
 	}
 	return 0;
 }
+
+/**----output----**/
+
+
 int getWindowSize(unsigned short int *rows, unsigned short int *cols)
 {
 	struct winsize ws;
@@ -262,7 +363,6 @@ int getWindowSize(unsigned short int *rows, unsigned short int *cols)
 			return -1;	
 		}
 		int k = getCursorPosition(rows, cols);
-		// printf("cols %d, rows %d\r\n", *cols, *rows);
 		err("WTF");
 		return k;
 		
@@ -270,30 +370,73 @@ int getWindowSize(unsigned short int *rows, unsigned short int *cols)
 	else {
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
-		// printf("cols %d, rows %d\r\n", *cols, *rows);
 		return 0;
 	}
-	// *rows = 24;
-	// *cols = 35;
-	// return 0;
 
 	
 }
+
+
+void scroll()
+{
+	if(config.cy<config.rowOff)	
+	{
+		config.rowOff = config.cy;
+	}
+	if(config.cy >= config.rowOff + config.screenrows)
+	{
+		config.rowOff = config.cy-config.screenrows + 1;
+	}
+	if(config.cx < config.colOff)
+	{
+		config.colOff = config.cx;
+	}
+	if(config.cx >= config.colOff + config.screencols)
+	{
+		config.colOff = config.cx-config.screencols + 1;
+	}
+
+}
+/**
+ * @brief This function draws the rows of the editor
+ * 
+ * @param ab 
+ */
 void drawRows(abuf *ab)
 {
 	for(int y = 0; y<config.screenrows; y++)
 	{
-		if(y  == 0)
+		int filerow = y+config.rowOff;
+		if(filerow >= config.numrow)
 		{
-			char welcomemsg[80] = {0};
-			int msglen = snprintf(welcomemsg, sizeof(welcomemsg), "TextEditor Version %s", VER);
-			if(msglen > config.screencols) msglen = config.screencols;
-			abAppend(ab, welcomemsg, msglen);
+			if(y>=config.numrow)
+			{
+				if(config.numrow == 0 && y == config.screenrows/3)
+				{
+					char welcomemsg[80] = {0};
+					int msglen = snprintf(welcomemsg, sizeof(welcomemsg), "TextEditor Version %s", VER);
+					if(msglen > config.screencols) msglen = config.screencols;
+					int padding = (config.screencols - msglen) / 2;
+					if (padding) {
+					abAppend(ab, "~", 1);
+					padding--;
+					}
+					while (padding--) abAppend(ab, " ", 1);
+					abAppend(ab, welcomemsg, msglen);
 
+				}
+				else
+				{
+					abAppend(ab, "~", 1);
+				}
+			}
 		}
 		else
 		{
-		abAppend(ab, "~", 1);
+			int len = config.row[filerow].size - config.colOff;
+			if(len<0) len = 0;
+			if(len > config.screencols) len = config.screencols;
+			abAppend(ab, &config.row[filerow].chars[config.colOff], len);
 		}
 		abAppend(ab, "\x1b[K", 3); //For clearing one line at a time
 		if(y < config.screenrows-1)
@@ -303,42 +446,59 @@ void drawRows(abuf *ab)
 		
 	}
 }
+
+/**
+ * @brief This function refreshes the screen after every keypress
+ * 
+ */
 void refreshScreen()
 {
+	scroll();
 	abuf ab = ABUF_INIT;
 	//0x1b-27
 
 	// //2 clears entire screen 0-till cursor 1-cursor till end
 	// abAppend(&ab, "\x1b[2J", 4);
-
+	abAppend(&ab, "\x1b[?25l", 6);
 	//Positioning cursor at beginning
 	abAppend(&ab,  "\x1b[H", 3);
 
 	//then drawing
 	drawRows(&ab);
 	char buf[32];
-	int length = snprintf(buf, sizeof(buf), "\x1b[%d;%dH", config.cy+1, config.cx + 1);
+	int length = snprintf(buf, sizeof(buf), "\x1b[%d;%dH",
+					(config.cy - config.rowOff+1), (config.cx - config.colOff + 1));
 	if(length  == 0) err("Cursor error");
 	abAppend(&ab, buf, strlen(buf));
 
-
+	abAppend(&ab, "\x1b[?25h", 6);
 	write(STDOUT_FILENO, ab.s, ab.len);
-	// printf("\r\n%d %d", config.cx, config.cy);
 	abFree(&ab);
 }
+
+
 /**init**/
 
-//Responsible for initialising the editor's state structure
+/**
+ * @brief This function initializes the editor
+ * 
+ */
 void initEditor() 
 {
 	config.cx = config.cy = 0;
+	config.numrow = 0;
+	config.row = NULL;
+	config.rowOff = config.colOff = 0;
 	if(getWindowSize(&config.screenrows, &config.screencols) == -1) err("getWindowSize");
 }
-int main()
+int main(int argc, char *argv[])
 {
 	enableRawMode();
 	initEditor();
-	// printf("%d %d\r\n", config.screencols, config.screenrows);
+	if(argc >= 2)
+	{
+		editorOpen(argv[1]);
+	}
 	while(1)
 	{
 		refreshScreen();
