333384fa297af19bf899f4e17c7d7a8466dcb68f
Witty-31-06 <92244610+Witty-31-06@users.noreply.github.com>
2024-02-13 18:34:48 +0530

    Added feature for editing existing file, saving new file.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #define _GNU_SOURCE
 #define VER "1.0"
 #define TABSIZE 4
-
+#define QUIT_TIME 2
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -14,6 +14,7 @@
 #include <sys/ioctl.h>
 #include <time.h>
 #include <stdarg.h>
+#include <fcntl.h>
 
 typedef struct termios terminal;
 
@@ -45,6 +46,7 @@ typedef enum keys{
     HOME = 1006,
     END = 1007,
     DEL = 1008,
+    BACKSPACE = 127,
 } splKeys;
 
 
@@ -58,6 +60,7 @@ typedef struct estate
     int colOff;
     int rowOff; //for scrolling
     erow *row; //Stores a row of text from the file
+    int dirty;
     char *filename;
     char statusMsg[80];
     time_t statusMsgTime;
@@ -65,6 +68,11 @@ typedef struct estate
 } editorState;
 editorState config;
 
+
+/***prototype*/
+void setStatusMsg(const char *fmt, ...);
+void refreshScreen();
+char *promptUser(char *prompt);
 
 /**
  * @brief Outputs the error in screen and exits
@@ -168,26 +176,177 @@ void updateRow(erow *row) {
   row->render[idx] = '\0';
   row->rsize = idx;
 }
-void appendRow(char *s, size_t len)
+void insertRow(int pos, char *s, size_t len)
 {
+    if(pos < 0 || pos > config.numrow) return;
+    config.row = realloc(config.row, sizeof(erow) * (config.numrow + 1));
+    memmove(&config.row[pos + 1], &config.row[pos], sizeof(erow) * (config.numrow-pos));
     //Adding one extra space for new row
     config.row = realloc(config.row, (sizeof(erow)) * (config.numrow+1));
 
-    //Inserting the row info at the specific index
-    int ind = config.numrow;
-    config.row[ind].size = len;
-    config.row[ind].chars = malloc(len+1);
-    strncpy((config.row[ind].chars), s, len);
+    //Inserting the row info at the appropriate index
+    config.row[pos].size = len;
+    config.row[pos].chars = malloc(len+1);
+    strncpy((config.row[pos].chars), s, len);
 
 
-    config.row[ind].rsize = 0;
-    config.row[ind].render = NULL;
-    updateRow(&config.row[ind]);
+    config.row[pos].rsize = 0;
+    config.row[pos].render = NULL;
+    updateRow(&config.row[pos]);
     config.numrow++;
+    config.dirty++;
 
+}
+void rowDelete(erow *row, int pos)
+{
+    if(pos<0 || pos>=row->size) return; //Invalid position
+    memmove(&row->chars[pos], &row->chars[pos + 1], row->size - pos);
+    row->size--;
+    updateRow(row);
+    config.dirty++;
+}
+void rowInsertChar(erow *row, int pos, int c)
+{
+    if(pos<0 || pos>row->size) pos = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    //Shift the content one place right,eg H|ello becomes H_ello | is cursor
+    memmove(&row->chars[pos + 1], &row->chars[pos], row->size- pos + 1);
+    row->size++;
+    row->chars[pos] = c;
+    updateRow(row);
+    config.dirty++;
+}
+
+
+
+/**editor operations**/
+
+void freeRow(erow *row)
+{
+    free(row->chars);
+    free(row->render);
+}
+void DelRow(int pos)
+{
+    if(pos<0 || pos >= config.numrow) return;
+    freeRow(&config.row[pos]); //freeing the current line
+    memmove(&config.row[pos], &config.row[pos+1], sizeof(erow) * (config.numrow - pos -1));
+    config.numrow--;
+    config.dirty++;
+}
+
+void joinRows(erow *row, char *s, size_t len)
+{
+    row->chars = realloc(row->chars, row->size + len + 1);
+    strncpy(&row->chars[row->size], s, len);
+    row->size += len;
+    updateRow(row);
+    config.dirty++;
+
+}
+void DelChar()
+{
+    if(config.cy == config.numrow) return; //last line do nothing
+    if(config.cx == 0 && config.cy == 0) return; //top left of screen
+    erow *row = &config.row[config.cy]; //ptr to current row
+    if(config.cx > 0)
+    {
+        rowDelete(row, config.cx - 1);
+        config.cx--;
+    }
+    else
+    {
+        config.cx = config.row[config.cy-1].size; //x becomes the size of prev line
+        joinRows(&config.row[config.cy-1], row->chars, row->size);
+        DelRow(config.cy);
+        config.cy--;
+    }
+}
+void insertChar(int c)
+{
+    if(config.cy == config.numrow)
+    {
+        insertRow(config.numrow, "", 0);
+    }
+    rowInsertChar(&config.row[config.cy], config.cx, c);
+    config.cx++;
+}
+
+void insertNewLine()
+{
+    if(config.cx == 0) //cursor at beginning of line
+    {
+        insertRow(config.cy, "", 0);
+    }
+    else
+    {
+        erow *row = &config.row[config.cy];  //ptr to current row
+        insertRow(config.cy + 1, &row->chars[config.cx], row->size - config.cx); //inserted new row
+        row = &config.row[config.cy];
+        row->size = config.cx; //trimming the current row
+        row->chars[row->size] = '\0'; //inserting null char
+        updateRow(row);
+    }
+    config.cy++;
+    config.cx = 0;
 }
 /**FILE I/O**/
 
+char *rowToString(int *buflen)
+{
+    int totlen = 0; //Total length of the file
+    int j;
+    for(j = 0; j<config.numrow; j++)
+    {
+        totlen += config.row[j].size + 1; //extra space for \n
+    }
+    *buflen = totlen;
+    char *buf = malloc(totlen); //allocating one big chunk of memory
+    char *p = buf;
+    for (j = 0; j < config.numrow; j++) {
+        memcpy(p, config.row[j].chars, config.row[j].size);
+        p += config.row[j].size; //incrementing p to the end
+        *p = '\n'; //Adding newline at the end
+        p++;
+    }
+    return buf;
+}
+
+void saveFile()
+{
+    if(config.filename == NULL)
+    {
+        config.filename = promptUser("Save as: %s (ESC to Cancel)");
+        if(config.filename == NULL)
+        {
+            setStatusMsg("Save Aborted");
+        }
+        return;
+    }
+    int len;
+    char *buf = rowToString(&len);
+    //Permissions: user|group|other 06-rw 04-r 04-r ->0644
+    int fd = open(config.filename, O_RDWR | O_CREAT, 0644);
+    if(fd != -1)
+    {
+        if(ftruncate(fd, len) != -1)
+        {
+            if(write(fd, buf, len) == len)
+            {
+                close(fd);
+                free(buf);
+                config.dirty = 0;
+                setStatusMsg("%d bytes written to disk", len);
+                return;
+            }
+        }
+        close(fd);
+    }
+    free(buf);
+    
+    setStatusMsg("Can't save. I/O error: %s", strerror(errno));
+    return;
+}
 /**
  * @brief This function opens the file
  *
@@ -207,10 +366,11 @@ void editorOpen(char *filename)
         {
             linelen--;
         }
-        appendRow(line, linelen); //Insert the line in row struct
+        insertRow(config.numrow, line, linelen); //Insert the line in row struct
     }
 
     free(line);
+    config.dirty = 0;
     fclose(fp);
 }
 /**Append Buffer**/
@@ -309,6 +469,48 @@ int readKey()
 }
 
 
+char *promptUser(char *prompt)
+{
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize); //Stores user input
+    size_t buflen = 0;
+    buf[0] = 0;
+
+    while(1)
+    {
+        setStatusMsg(prompt, buf);
+        refreshScreen();
+        int c = readKey();
+        if(c == DEL || c == CTRL('h') || c == BACKSPACE)
+        {
+            if(buflen != 0) buf[--buflen] = 0; //Inserting NULL at the position before
+        }
+        else if(c == '\r') //if enter is pressed
+        {
+            if(buflen != 0)
+            {
+                setStatusMsg("");
+                return buf;
+            }
+        }
+        else if(c == '\x1b') //Esc to cancel
+        {
+            setStatusMsg("");
+            free(buf);
+            return NULL;
+        }
+        else if(!iscntrl(c) && c<128)
+        {
+            if(buflen == bufsize -1)
+            {
+                bufsize = bufsize*2; //Increase buffer size when overflow
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = 0;
+        }
+    }
+}
 /**
  * @brief This function moves the cursor
  *
@@ -368,15 +570,26 @@ void moveCursor(int key)
  */
 void processKeyPress()
 {
+    static int quit_time = QUIT_TIME;
     int c = readKey();
     switch(c)
     {
         //present in ttydefaults already included
         case CTRL('q'):
+            if(config.dirty && quit_time>0)
+            {
+                setStatusMsg("WARNING !! Unsaved changes. "
+                "Press ctrl-q %d more times.", quit_time);
+                quit_time--;
+                return;
+            }
             //clear screen then exit
             write(STDOUT_FILENO, "\x1b[2J", 4);
-              write(STDOUT_FILENO, "\x1b[H", 3);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+        case CTRL('s'):
+            saveFile();
+            break;
         case HOME:
             config.cx = 0;
             break;
@@ -403,9 +616,22 @@ void processKeyPress()
         case ARROW_RIGHT:
             moveCursor(c);
             break;
+        case '\r':
+            insertNewLine();
+            break;
+        case BACKSPACE:
+        case CTRL('h'):
+        case DEL:
+            if(c == DEL) moveCursor(ARROW_RIGHT);
+            DelChar();
+            break;
+        case '\x1b':
+            break;
         default:
+            insertChar(c);
             break;
     }
+    quit_time = QUIT_TIME;
 }
 
 /**
@@ -558,8 +784,9 @@ void drawStatusBar(abuf *ab)
     char status[100];
     char lno[30]; //Shows line number
 
-    int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-        config.filename ? config.filename : "[Untitled]", config.numrow);
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+        config.filename ? config.filename : "[Untitled]", config.numrow,
+        config.dirty != 0 ? "(modified)" : "(Unmodified)");
     int rlen = snprintf(lno, sizeof(lno), "Ln %d, Col %d", config.cy+1, config.cx + 1);
     if(len > config.screencols) len = config.screencols;
     abAppend(ab, status , len);
@@ -580,6 +807,14 @@ void drawStatusBar(abuf *ab)
     abAppend(ab, "\x1b[0m", 4);
     abAppend(ab, "\r\n", 2);
 
+}
+void setStatusMsg(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(config.statusMsg, sizeof(config.statusMsg), fmt, ap);
+    va_end(ap);
+    config.statusMsgTime = time(NULL);
 }
 
 void drawMsgBar(abuf *ab)
@@ -645,14 +880,7 @@ void refreshScreen()
     write(STDOUT_FILENO, ab.s, ab.len);
     abFree(&ab);
 }
-void setStatusMsg(const char *fmt, ...)
-{
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(config.statusMsg, sizeof(config.statusMsg), fmt, ap);
-    va_end(ap);
-    config.statusMsgTime = time(NULL);
-}
+
 
 /**init**/
 
@@ -670,6 +898,7 @@ void initEditor()
     config.filename = NULL;
     config.statusMsg[0] = 0;
     config.statusMsgTime = 0;
+    config.dirty = 0;
     if(getWindowSize(&config.screenrows, &config.screencols) == -1) err("getWindowSize");
     config.screenrows -= 2; //Making two empty space at bottom of the screen
 }
@@ -681,7 +910,7 @@ int main(int argc, char *argv[])
     {
         editorOpen(argv[1]);
     }
-    setStatusMsg("HELP: Ctrl-Q = quit");
+    setStatusMsg("HELP: Ctrl-Q = quit | Ctrl-S = save");
     while(1)
     {
         refreshScreen();
